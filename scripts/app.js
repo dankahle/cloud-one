a51bb38f9847ab97eead72031ec5e3aa8b4da88f
Dan Kahle <dan.kahle@gmail.com>
2014-06-16 10:40:27 -0700

    build 6-16-14

---
+++ scripts/app.js
@@ -0,0 +1,25 @@
+/// <reference path="../bower_components/angular/angular.js" />
+"use strict";
+
+var app = angular.module("app", []);
+
+app.controller("ctrl", function ($scope, $http) {
+
+/*
+	$scope.users = [
+		{ name: "dank", age: 50 },
+		{ name: "carl", age: 60 }
+	];
+*/
+
+
+	$http.get(dkGetServiceUrl() + "api/User").success(function (data) {
+		$scope.users = data;
+	});
+
+});
+
+
+
+
+
